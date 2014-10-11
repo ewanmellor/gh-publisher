04996706d2206fc0a34c9c3678960d4cecc87919
Ewan Mellor <ewan@tipbit.com>
2014-10-11 14:44:56 -0700

    Publish any .gif, .jpeg, .jpg, .png that we find.

--- gh-publisher-scripts/copy.sh
+++ gh-publisher-scripts/copy.sh
@@ -14,6 +14,10 @@ find . \( \
      -name \*.html -o \
      -name \*.css -o \
      -name \*.js -o \
+     -name \*.gif -o \
+     -name \*.jpeg -o \
+     -name \*.jpg -o \
+     -name \*.png -o \
      -name \*.pdf \
      \) -print0 |
     rsync -av --files-from=- --from0 ./ "$GH_PUBLISHER_PUBLISH_DIR"
